42d2749e522311f8653c53a8baa77b61276ad416
BK <79825101+Bhushan8177@users.noreply.github.com>
2022-01-09 11:56:47 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,114 @@
 import React from 'react';
 import './App.css';
 
-function About() {
-  return (
-    <div>
-      <h1>About</h1>
-
+function About(){ 
+    return (
+    <>
+  
+  <div>
+    <h1 className="first">Who we are and why <br></br>we are doing this</h1>
+  </div>
+  <div>
+    <h6 className="second">Now that your eyes are open, make the sun jeolous,<br></br> cvvsvds vsds,cwrevevev,evevvv </h6>
+  </div>
+  <div className="aboutmain">
+    <div className="testimonial-grid">
+      <article className="testimonial ">
+        <div className="flex">
+          <div>
+            {/* <img  src="./images/image-daniel.jpg" alt="daniel clifford"/> */}
+          </div>
+          <div>
+            <h2 className="name"><strong>Who are We</strong></h2>
+          </div>
+        </div>
+        <p>
+          we invite you to an anniversary party to celebrate their ten years of marriage!!!!
+        </p>
+      </article>
+      <article className="testimonial flow bg-secondary-400 text-neutral-100">
+        <div className="flex">
+          <div>
+            {/* <img src="./images/image-jonathan.jpg" alt="Jonathan Walters"/> */}
+          </div>
+          <div>
+            <h2 className="name"><strong>Mission</strong></h2>
+          </div>
+        </div>
+        <p>
+        we invite you to an anniversary party to celebrate their ten years of marriage!!!!
+        </p>
+      </article>
+      <div className="box3">
+       <article className="testimonial flow bg-neutral-100 text-secondary-400">
+        <div className="flex">
+          <div>
+            {/* <img src="./images/image-jeanette.jpg" alt="Jeanette Harmon"/> */}
+          </div>
+          <div>
+            <h2 className="name"><strong>Vision</strong></h2>
+    
+          </div>
+        </div>
+        <p>
+        we invite you to an anniversary party to celebrate their ten years of marriage!!!!
+        </p>
+      </article>
+      </div>
+      <div className="box4">
+      
+      <article className="testimonial grid-col-span-2 flow bg-secondary-500 text-neutral-100">
+        <div className="flex">
+          
+          <div>
+            <h2 className="name"><strong>50+</strong></h2>
+          </div>
+          <p className="sub-name">Members</p>
+        </div>
+        
+      </article>
+      <article className="testimonial flow bg-neutral-100 text-secondary-400">
+        <div className="flex">
+        
+          <div>
+            <h2 className="name"><strong>50+</strong></h2>   
+          </div>
+          <p className="sub-name">Alumni</p>
+        </div>
+       
+    
+      </article> 
+      <article className="testimonial grid-col-span-2 flow bg-secondary-500 text-neutral-100">
+        <div className="flex">
+          
+          <div>
+            <h2 className="name"><strong>50+</strong></h2>
+          </div>
+          <p className="sub-name">Projects</p>
+        </div>
+       
+       
+      </article>
+      <article className="testimonial flow bg-neutral-100 text-secondary-400">
+        <div className="flex">
+        
+          <div>
+            <h2 className="name"><strong>50+</strong></h2>
+          </div>
+          <p className="sub-name">Achievement</p>
+        </div>
+    
+      </article> 
+      </div>
     </div>
-  );
-}
+  
+  
+  
+  
+  </div>
+</>
 
-export default About;
+)}
+
+
+export default About
