9639af656570d9bf06e355bd2438ecc190f2bc24
Akanksha☺️ <89179275+Enthusiast22@users.noreply.github.com>
2022-01-09 12:30:31 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,114 +1,192 @@
-import React from 'react';
-import './App.css';
+
+  
+import React from "react";
+import "./About.css";
+
+
 
 function About(){ 
     return (
     <>
   
-  <div>
-    <h1 className="first">Who we are and why <br></br>we are doing this</h1>
-  </div>
-  <div>
-    <h6 className="second">Now that your eyes are open, make the sun jeolous,<br></br> cvvsvds vsds,cwrevevev,evevvv </h6>
-  </div>
+  
   <div className="aboutmain">
     <div className="testimonial-grid">
-      <article className="testimonial ">
+      <article className="testimonial flow bg-secondary-400 text-neutral-100 ">
         <div className="flex">
           <div>
-            {/* <img  src="./images/image-daniel.jpg" alt="daniel clifford"/> */}
+            <img  src="./images/image-daniel.jpg" alt="daniel clifford"/>
           </div>
           <div>
-            <h2 className="name"><strong>Who are We</strong></h2>
+            <h2 className="name">Daniel Clifford</h2>
+            <p className="position">Verified Graduate</p>
           </div>
         </div>
         <p>
-          we invite you to an anniversary party to celebrate their ten years of marriage!!!!
+          I received a job offer mid-course, and the subjects I learned were current, if not more so,
+          in the company I joined. I honestly feel I got every penny’s worth.
+        </p>
+        <p>
+          “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
+          transition and have heard some people who had an amazing experience here. I signed up
+          for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
+          The next 12 weeks was the best - and most grueling - time of my life. Since completing
+          the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”
         </p>
       </article>
       <article className="testimonial flow bg-secondary-400 text-neutral-100">
         <div className="flex">
           <div>
-            {/* <img src="./images/image-jonathan.jpg" alt="Jonathan Walters"/> */}
+            <img src="./images/image-jonathan.jpg" alt="Jonathan Walters"/>
           </div>
           <div>
-            <h2 className="name"><strong>Mission</strong></h2>
+            <h2 className="name">Jonathan Walters</h2>
+            <p className="position">Verified Graduate</p>
           </div>
         </div>
         <p>
-        we invite you to an anniversary party to celebrate their ten years of marriage!!!!
+          The team was very supportive and kept me motivated
         </p>
-      </article>
-      <div className="box3">
-       <article className="testimonial flow bg-neutral-100 text-secondary-400">
-        <div className="flex">
-          <div>
-            {/* <img src="./images/image-jeanette.jpg" alt="Jeanette Harmon"/> */}
-          </div>
-          <div>
-            <h2 className="name"><strong>Vision</strong></h2>
-    
-          </div>
-        </div>
         <p>
-        we invite you to an anniversary party to celebrate their ten years of marriage!!!!
+          “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
+          for a big company. This was one of the best investments I’ve made in myself. ”
         </p>
       </article>
       </div>
-      <div className="box4">
+      <div className="row2">
+     
+       <article className="testimonial grid-col-span-2 flow bg-secondary-400 text-neutral-100s">
+        <div className="flex">
+          <div>
+            <img src="./images/image-jeanette.jpg" alt="Jeanette Harmon"/>
+          </div>
+          <div>
+            <h2 className="name">Jeanette Harmon</h2>
+            <p className="position">Verified Graduate</p>
+          </div>
+        </div>
+        <p>
+          An overall wonderful and rewarding experience</p>
+        <p>
+          “ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living
+          while doing something I love. ”
+        </p>
+      </article>
       
-      <article className="testimonial grid-col-span-2 flow bg-secondary-500 text-neutral-100">
+     
+      
+      <article className="testimonial  flow bg-secondary-500 text-neutral-100">
         <div className="flex">
           
           <div>
-            <h2 className="name"><strong>50+</strong></h2>
+            <h2 className="name">Patrick Abrams</h2>
+            <p className="position">Verified Graduate</p>
           </div>
-          <p className="sub-name">Members</p>
         </div>
         
       </article>
-      <article className="testimonial flow bg-neutral-100 text-secondary-400">
+      <article className="testimonial flow bg-secondary-400 text-neutral-100">
         <div className="flex">
         
           <div>
-            <h2 className="name"><strong>50+</strong></h2>   
+            <h2 className="name">Kira Whittle</h2>
+            <p className="position">Verified Graduate</p>
           </div>
-          <p className="sub-name">Alumni</p>
         </div>
        
     
       </article> 
-      <article className="testimonial grid-col-span-2 flow bg-secondary-500 text-neutral-100">
+     
+      <article className="testimonial  flow bg-secondary-500 text-neutral-100">
         <div className="flex">
           
           <div>
-            <h2 className="name"><strong>50+</strong></h2>
+            <h2 className="name">Patrick Abrams</h2>
+            <p className="position">Verified Graduate</p>
           </div>
-          <p className="sub-name">Projects</p>
         </div>
        
        
       </article>
-      <article className="testimonial flow bg-neutral-100 text-secondary-400">
+      <article className="testimonial flow bg-secondary-400 text-neutral-100">
         <div className="flex">
         
           <div>
-            <h2 className="name"><strong>50+</strong></h2>
+            <h2 className="name">Kira Whittle</h2>
+            <p className="position">Verified Graduate</p>
           </div>
-          <p className="sub-name">Achievement</p>
         </div>
     
       </article> 
       </div>
+    
+    
+    <div className="dept">
+        <div className="dept-title"><p>Our Departments</p></div>
+        <div className="dept-grid">
+        <article className="dept-grid-box flow bg-secondary-400 text-neutral-100">
+        
+          <div>
+            <img  src="./images/image-daniel.jpg" alt="daniel clifford"/>
+          </div>
+          <div>
+            <h2 className="name">Daniel Clifford</h2>
+            <p className="position">Verified Graduate</p>
+          </div>
+        
+        <p>
+          I received a job offer mid-course, and the subjects I learned were current, if not more so,
+          in the company I joined. I honestly feel I got every penny’s worth.
+        </p>
+        
+     
+      </article>
+      <article className="dept-grid-box flow bg-secondary-400 text-neutral-100">
+        
+        <div>
+          <img  src="./images/image-daniel.jpg" alt="daniel clifford"/>
+        </div>
+        <div>
+          <h2 className="name">Daniel Clifford</h2>
+          <p className="position">Verified Graduate</p>
+        </div>
+      
+      <p>
+        I received a job offer mid-course, and the subjects I learned were current, if not more so,
+        in the company I joined. I honestly feel I got every penny’s worth.
+      </p>
+      
+   
+    </article>
+    <article className="dept-grid-box flow bg-secondary-400 text-neutral-100">
+        
+        <div>
+          <img  src="./images/image-daniel.jpg" alt="daniel clifford"/>
+        </div>
+        <div>
+          <h2 className="name">Daniel Clifford</h2>
+          <p className="position">Verified Graduate</p>
+        </div>
+      
+      <p>
+        I received a job offer mid-course, and the subjects I learned were current, if not more so,
+        in the company I joined. I honestly feel I got every penny’s worth.
+      </p>
+      
+   
+    </article>
+      </div>
+      
     </div>
   
+    </div>
   
-  
-  
-  </div>
+ 
 </>
 
 )}
 
 
 export default About
+
+
